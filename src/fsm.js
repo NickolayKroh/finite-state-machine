abe3843e04cf527a5f444ea9c59f74d1c8938639
Kroh Nickolay <mikolka.del@gmail.com>
2019-09-29 17:59:27 +0300

    task complited

--- src/fsm.js
+++ src/fsm.js
@@ -1,32 +1,87 @@
+const conf = {
+    initial: 'normal',
+    states: {
+        normal: {
+            transitions: {
+                study: 'busy',
+            }
+        },
+        busy: {
+            transitions: {
+                get_tired: 'sleeping',
+                get_hungry: 'hungry',
+            }
+        },
+        hungry: {
+            transitions: {
+                eat: 'normal'
+            },
+        },
+        sleeping: {
+            transitions: {
+                get_hungry: 'hungry',
+                get_up: 'normal',
+            },
+        },
+    }
+};
+
 class FSM {
     /**
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if(!config)
+            throw new Error();
+        
+        this.initial = config.initial;
+        this.states = config.states;
+        this.reset();
+        this.history = [];
+        this.canceled = [];
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.stateActual;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if(!this.states[state])
+            throw new Error();
+        
+        this.history.push(this.stateActual);
+        this.canceled = [];
+        this.stateActual = state;
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        let newState = this.states[this.stateActual].transitions[event];
+
+        if(!this.states[newState])
+            throw new Error();
+
+        this.changeState(newState);
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.stateActual = this.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +89,56 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        if(!event)
+            return Object.keys(this.states);
+        
+        let result = [];
+        
+        for(let i = 0; i < Object.values(this.states).length; ++i)
+            if(event in Object.values(this.states)[i].transitions)
+                result.push( Object.keys(this.states)[i] );
+        
+        return result;
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if(!this.history.length)
+            return false;
+        
+        let state = this.history.pop();
+        this.canceled.push(this.getState());
+        this.stateActual = state;
+        return true;
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        if(!this.canceled.length)
+            return false;
+        
+        let state = this.canceled.pop();
+        this.history.push(state);
+        this.stateActual = state;
+        return true;
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.history = [];
+        this.canceled = [];
+    }
 }
 
 module.exports = FSM;
